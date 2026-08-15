1d808147ebbf871c4d6070e44122b9f3109469dd
Jamesm2009 <ZAFjmcc@yahoo.com>
2026-08-14 22:15:07 -0500

    Update data.js

--- api/data.js
+++ api/data.js
@@ -17,6 +17,7 @@ module.exports = async (req, res) => {
         category: node.category,
         summary: node.summary,
         keyMetric: node.keyMetric,
+        manualSources: node.manualSources || [],
         series: [],
         status: null,
       };
